42f520f6e151eae60d3243beafe9d03622cd09d6
Roman <rkonuhov@cogniance.com>
2016-11-09 00:32:29 +0300

    fixed Display component

--- app/components/Display.js
+++ app/components/Display.js
@@ -3,12 +3,17 @@ import _ from 'lodash';
 
 class Display extends React.Component {
   render() {
-    if (this.props.inline) {
-      return !_.isEmpty(this.props.if) ? (<span>{this.props.children}</span>) : null;
-    } else if (this.props.nowrap) {
-      return !_.isEmpty(this.props.if) ? this.props.children : null;
+    if (!(this.props.if instanceof Object) && this.props.if ||
+      (this.props.if instanceof Object) && !_.isEmpty(this.props.if) ) {
+      if (this.props.inline) {
+        return (<span>{this.props.children}</span>);
+      } else if (this.props.nowrap) {
+        return this.props.children
+      } else {
+        return (<div>{this.props.children}</div>);
+      }
     } else {
-      return !_.isEmpty(this.props.if) ? (<div>{this.props.children}</div>) : null;
+      return null;
     }
   }
 }
