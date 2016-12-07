f6111c601c1e76b99ab938c499b9b4cde39a2895
Roman <rkonuhov@cogniance.com>
2016-12-07 10:22:00 +0300

    fixed bash script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+apt-get install -y nodejs
 mkdir uploads
-cd db && node init.js
+chown -R rkonuhov:cogniance uploads
+node db/init.js
+chown -R rkonuhov:cogniance logs
+apt-get install mongodb
+npm -g i webpack
+npm -g i bower
+npm i
+bower install
